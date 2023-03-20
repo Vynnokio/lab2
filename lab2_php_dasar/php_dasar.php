06f9651a753ceb2eeb25c9fd29949d25f7d13de3
Vynnokio <127071734+Vynnokio@users.noreply.github.com>
2023-03-20 19:59:01 +0700

    Add files via upload

---
+++ lab2_php_dasar/php_dasar.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Dasar</title>
+</head>
+<body>
+<h1>Belajar PHP Dasar</h1>
+<?php
+echo "Hello World";
+?>
+<h1>Menggunakan Variable</h1>
+<?php
+$nim = "0411500400";
+$nama = 'Abdullah';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
+<h2>Form Input</h2>
+<form method="post">
+<label>Nama: </label>
+<input type="text" name="nama">
+<input type="submit" value="Kirim">
+</form>
+<?php
+echo 'Selamat Datang ' . $_POST['nama'];
+?>
+<?php
+$gaji = 1000000;
+$pajak = 0.1;
+$thp = $gaji - ($gaji*$pajak);
+echo "Gaji sebelum pajak = Rp. $gaji <br>";
+echo "Gaji yang dibawa pulang = Rp. $thp";
+?>
+<?php
+$nama_hari = date("l");
+if ($nama_hari == "Sunday") {
+echo "Minggu";
+} elseif ($nama_hari == "Monday") {
+echo "Senin";
+} else {
+echo "Selasa";
+}
+?>
+</body>
+</html>
